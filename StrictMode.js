d6996ff0bb8b7edfb308d46a11a377044d17bd95
Piotr Kowalski <kowalskii1@op.pl>
2018-05-07 09:10:43 +0200

    strict mode, .jshint file

---
+++ StrictMode.js
@@ -0,0 +1,11 @@
+// We can use Strict Mode globally
+// What is Strict Mode? = "JS is trying to help you, don't let it"
+// 'use strict';
+var x = 23;
+
+var func = function() {
+    // We can use it only in function scope
+    'use strict'
+}
+
+console.log(x);
